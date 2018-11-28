267a087341244e7964995eb14104b8282d457342
MMuttik <mmuttik@live.co.uk>
2018-11-28 18:11:47 +0000

    updated styling and length

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2018_11_26_163605) do
   enable_extension 'plpgsql'
 
   create_table 'users', force: :cascade do |t|
-    t.string 'email', default: ', null: false
-    t.string 'encrypted_password', default: ', null: false
-    t.string 'name', default: ', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'name', default: ''', null: false
     t.string 'reset_password_token'
     t.datetime 'reset_password_sent_at'
     t.datetime 'remember_created_at'
