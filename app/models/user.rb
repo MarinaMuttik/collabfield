15be14cea9176a9c295f68f5733f319cd009dcf9
MMuttik <mmuttik@live.co.uk>
2018-12-02 22:59:12 +0000

    generate post and category models create assoc between user, post, category update category and post tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # destroy deletes all posts user made if the user is destroyed
+  has_many :posts, dependent: :destroy
 end
