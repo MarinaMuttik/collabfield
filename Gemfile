59ffe9c90a319dfc8609fc67a24c81a9cce64041
MMuttik <mmuttik@live.co.uk>
2018-12-03 16:09:00 +0000

    added faker, updated homepage, generated user,cat,post records in seeds.rb

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'faker'
 end
 
 group :test do
@@ -68,6 +69,8 @@ group :test do
   gem 'headless'
   gem 'poltergeist'
   gem 'database_cleaner'
+  # generate dummy text
+  gem 'faker'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def seed_users
+  user_id = 1
+  10.times do
+    User.create(
+      name: "test#{user_id}",
+      email: "test#{user_id}@test.com",
+      password: '123456',
+      password_confirmation: '123456'
+    )
+    user_id += 1
+  end
+end
+
+Categories = {
+  'hobby' => ['Arts', 'Sports', 'Sciences', 'Collecting', 'Reading', 'Other'],
+  'study' => ['Arts and Humanities', 'Physical Science and Engineering',
+           'Math and Logic', 'Computer Science', 'Data Science',
+           'Economics and Finance', 'Business', 'Social Sciences',
+           'Language', 'Law', 'Other'],
+  'team' => ['Study', 'Development', 'Arts and Hobby', 'Other']
+}
+
+def seed_categories
+  Categories.each do |branch, name|
+    Category.create(branch: branch, name: name)
+  end
+end
+
+
+def seed_posts
+  categories = Category.all
+
+  categories.each do |category|
+    5.times do
+      Post.create(
+        title: Faker::Lorem.sentences[0],
+        content: Faker::Lorem.sentences[0],
+        user_id: rand(1..10),
+        category_id: category.id
+      )
+    end
+  end
+end
+
+seed_users
+seed_categories
+seed_posts
