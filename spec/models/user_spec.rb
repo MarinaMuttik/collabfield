7d6ded9dab93b5b28576d3ae61b264d79a67848b
MMuttik <mmuttik@live.co.uk>
2018-12-14 21:40:10 +0000

    added user validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe User, type: :model do
       expect(association.options[:dependent]).to eq :destroy
     end
   end
+
+  context 'Validations' do
+    let(:user1) { build(:user) }
+    let(:user2) { User.new }
+
+    # test new account without username
+    it "is not valid without a username" do
+      user1.name = nil
+      expect(user1).not_to be_valid
+    end
+
+    # test duplicate usernames
+    it 'can only create unique user names' do
+      user1.name = 'test1'
+      user2.name = 'test1'
+      expect(user1).to be_valid
+      expect(user2).not_to be_valid
+    end
+  end
 end
