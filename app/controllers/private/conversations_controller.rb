bd91d0ffeb083f8bfa24b821566028593b87bf2b
MMuttik <mmuttik@live.co.uk>
2018-12-18 17:11:08 +0000

    added close action in conv controller, added js partial

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -19,7 +19,15 @@ class Private::ConversationsController < ApplicationController
         format.js { render partial: 'posts/show/contact_user/message_form/failure' }
       end
     end
+  end
 
+  def close
+    @conversation_id = params[:id].to_i
+    session[:private_conversations].delete(@conversation_id)
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   private
